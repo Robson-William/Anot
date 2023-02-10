5191eb3845a6e606db891b40d1a4fbc4fab75ff8
Robson William <robsonwilliamro@gmail.com>
2023-02-10 13:57:16 -0300

    Adicionando neo4j ao projeto

---
+++ src/db/neo4j.mjs
@@ -0,0 +1,13 @@
+// Importando módulos
+import neo4j from 'neo4j';
+import * as dotenv from 'dotenv';
+
+// Configurando
+dotenv.config();
+
+let driver = neo4j.driver(
+	process.env.NEO4JHOST,
+	neo4j.auth.basic(process.env.NEO4JUSER, process.env.NEO4JPASSWORD)
+)
+
+export {driver};
